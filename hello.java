070894db1ad08168676b0674dae3e77794428dbe
kamalveerjala <110730199+kamalveerjala@users.noreply.github.com>
2022-08-06 23:54:30 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+class hello
+{
+  public statc void main(String args[])
+  {
+    System.out.println("hello");
+    int a;
+    for(a=10;a<21;a++)
+    {
+      System.out.println(a)
+      }
+  }
+}
